ddccfc10255dbefd4611a458accbd31aa6b7c53e
Mary Do <mdo5128@gmail.com>
2022-06-17 12:37:21 -0500

    Create keymap.c

---
+++ marydodo/keymap.c
@@ -0,0 +1,93 @@
+// ~~~~~~ Custom Keymap by marydodo ~~~~~
+
+#include QMK_KEYBOARD_H
+
+extern keymap_config_t keymap_config;
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _BASE 0
+#define _FN1 1
+
+#define CLIP_B LCTL(LSFT(KC_C))
+#define NEW_TAB LGUI(KC_T)
+#define NEW_INCOG LGUI(LSFT(KC_N))
+#define TERM_ESC LCTL(KC_C)
+#define COPY_C LGUI(KC_C)
+#define PASTE_V LGUI(KC_V)
+#define ALFRED_O LCTL(KC_SPC)
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+};
+
+// Alpha layer
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT_65_with_macro(
+    KC_ESC,     ALFRED_O,   KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_MUTE, \
+    NEW_TAB,    COPY_C,     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
+    NEW_INCOG,  PASTE_V,    KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
+    KC_END,     NEW_TAB,    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   RGB_SPI, \
+    TERM_ESC,   CLIP_B,     KC_LCTL,  KC_LALT, KC_LGUI, KC_LCTL, KC_SPC,  KC_SPC, KC_SPC, KC_RGUI, KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
+  ),
+
+// FN, RESET, and RGB mod layer
+  [_FN1] = LAYOUT_65_with_macro(
+    RGB_MODE_FORWARD, _______, KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, _______, \
+    _______, _______, RGB_TOG, RGB_MOD, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, KC_TILD, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, RESET
+  )
+};
+
+// REMEMBER TO ADD #define RGBLIGHT_LAYERS in your config.h file
+
+// Light LEDs on left half red when caps lock is active. Hard to ignore!
+const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 4, HSV_RED},       // Light 4 LEDs, starting with LED 6
+    {9, 4, HSV_RED}       // Light 4 LEDs, starting with LED 12
+);
+
+// Now define the array of layers. Later layers take precedence
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    my_capslock_layer
+);
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    // Both layers will light up if both kb layers are active
+    rgblight_set_layer_state(1, layer_state_cmp(state, 1));
+    return state;
+}
+
+bool led_update_user(led_t led_state) {
+    rgblight_set_layer_state(0, led_state.caps_lock);
+    return true;
+}
+
+
+// ENCODER SETTINGS
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    }
+    else if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    }
+    return true;
+}
